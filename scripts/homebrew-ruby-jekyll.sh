63d80859cb4c844644c84c80b9d6cec5344ee6bb
Sebastien Saunier <seb@saunier.me>
2014-07-29 12:26:10 +0200

    Added rbenv configuration

--- scripts/homebrew-ruby-jekyll.sh
+++ scripts/homebrew-ruby-jekyll.sh
@@ -2,21 +2,22 @@
 
 set -e
 
-echo "Step 1/6 - Installing Homebrew"
+echo "Step 1/5 - Installing Homebrew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/homebrew/go/install)"
 
-echo "Step 2/6 - Installing git"
-brew install git
-
-echo "Step 3/6 - Installing oh-my-zsh"
+echo "Step 2/5 - Installing oh-my-zsh"
 curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
 
-echo "Step 4/6 - Installing rbenv"
+echo "Step 3/5 - Installing rbenv"
 curl https://raw.githubusercontent.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
+echo 'export RBENV_ROOT="${HOME}/.rbenv"' >> ${HOME}/.zshrc
+echo 'export PATH="${RBENV_ROOT}/bin:${PATH}"'  >> ${HOME}/.zshrc
+echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi'  >> ${HOME}/.zshrc
+. ~/.zshrc
 
-echo "Step 5/6 - Installing Ruby 2.1.2"
+echo "Step 4/5 - Installing Ruby 2.1.2"
 rbenv install 2.1.2
 rbenv global 2.1.2
 
-echo "Step 6/6 - Installing Jekyll, Finally!!!"
+echo "Step 5/5 - Installing Jekyll, Finally!!!"
 gem install github-pages
