8ff6817e7e58d3443436c3d876f1c3c1504a105e
Sebastien Saunier <seb@saunier.me>
2014-10-09 17:26:26 +0200

    Uninstall zsh

--- scripts/homebrew-ruby-jekyll.sh
+++ scripts/homebrew-ruby-jekyll.sh
@@ -6,6 +6,7 @@ echo "Step 1/5 - Installing Homebrew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/homebrew/go/install)"
 
 echo "Step 2/5 - Installing oh-my-zsh"
+uninstall_oh_my_zsh || true
 curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
 
 zsh
