5ab3b72c9813e1dec5d69f74c93e5cc87d834098
Sebastien Saunier <seb@saunier.me>
2014-10-09 17:21:50 +0200

    Update the jekyll install script

--- scripts/homebrew-ruby-jekyll.sh
+++ scripts/homebrew-ruby-jekyll.sh
@@ -8,14 +8,17 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/homebrew/go/ins
 echo "Step 2/5 - Installing oh-my-zsh"
 curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
 
+zsh
+
 echo "Step 3/5 - Installing rbenv"
-curl https://raw.githubusercontent.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
 echo 'export RBENV_ROOT="${HOME}/.rbenv"' >> ${HOME}/.zshrc
 echo 'export PATH="${RBENV_ROOT}/bin:${PATH}"'  >> ${HOME}/.zshrc
 echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi'  >> ${HOME}/.zshrc
 echo 'export LANG=en_US.UTF-8'  >> ${HOME}/.zshrc
 echo 'export LC_ALL=en_US.UTF-8'   >> ${HOME}/.zshrc
-. ${HOME}/.zshrc
+source ${HOME}/.zshrc
+
+curl https://raw.githubusercontent.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
 
 echo "Step 4/5 - Installing Ruby 2.1.2"
 rbenv install 2.1.2
